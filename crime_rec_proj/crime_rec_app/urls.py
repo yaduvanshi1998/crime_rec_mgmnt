67c13d6c3f1717fb6b8a332570b3a5b5b94aee79
yaduvanshi1998 <73590070+yaduvanshi1998@users.noreply.github.com>
2024-06-20 12:37:37 -0400

    created django project, app and redirected url of project to url of app

--- crime_rec_proj/crime_rec_app/urls.py
+++ crime_rec_proj/crime_rec_app/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from crime_rec_app import views
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("", include("crime_rec_app.urls")),
+    path("", views.login, name = 'redirectlogin'),
+    path("login", views.login, name = 'login'),
 ]
