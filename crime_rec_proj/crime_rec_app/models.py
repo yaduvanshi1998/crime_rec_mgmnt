fc61320fb4c418f930fb9738bc5073739a9eec50
yaduvanshi1998 <73590070+yaduvanshi1998@users.noreply.github.com>
2024-06-21 12:14:16 -0400

    changed datetime to auto_now_add

--- crime_rec_proj/crime_rec_app/models.py
+++ crime_rec_proj/crime_rec_app/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Login_info(models.Model):
     Username = models.CharField(primary_key= True, max_length=20)
     Password = models.CharField(max_length=20)
-    DateTime = models.DateField(auto_now= True)
+    DateTime = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.Username
